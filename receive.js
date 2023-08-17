da26dfc8402fc9d9049e487d3651ae036b776c2c
LloydJanseVanRensburg <thefullstackjunkie@gmail.com>
2023-08-18 00:13:41 +0200

    done with the initial tutorial on basic hallo world send and receive

--- receive.js
+++ receive.js
@@ -1,6 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import amqp from 'amqplib/callback_api.js';
 
-amqp.connect('amqp://yourusername:yourpassword@localhost:5672', {},function(err1, con) {
+const yourName = process.env.MQ_USERNAME || 'yourusername';
+const yourPassword = process.env.MQ_USERNAME || 'yourusername';
+
+amqp.connect(`amqp://${yourName}:${yourPassword}@localhost:5672`, {},function(err1, con) {
     if(err1) throw err1;
 
     con.createChannel(function(err2, channel) {
